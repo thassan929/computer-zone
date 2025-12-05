99f3e43e1b1da4cf822e900affebfb7e80c643d5
thassan929 <thassan@algomau.ca>
2025-12-05 15:01:23 +0500

    Update products description

--- app/Views/pages/public_products.php
+++ app/Views/pages/public_products.php
@@ -3,8 +3,8 @@
 <div class="bg-white">
     <main class="mx-auto max-w-2xl px-4 lg:max-w-7xl lg:px-8">
         <div class="border-b border-gray-200 pt-24 pb-10">
-            <h1 class="text-4xl font-bold tracking-tight text-gray-900">New</h1>
-            <p class="mt-4 text-base text-gray-500">Checkout out the latest release of Basic Tees, new and improved with four openings!</p>
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900">Products</h1>
+            <p class="mt-4 text-base text-gray-500">Checkout out the latest prices!</p>
         </div>
 
         <div class="pt-12 pb-24 lg:grid lg:grid-cols-3 lg:gap-x-8 xl:grid-cols-4">
